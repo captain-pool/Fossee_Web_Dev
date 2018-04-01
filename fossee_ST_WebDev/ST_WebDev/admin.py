e5c8f3cbc7060000a918159ddebe89fd886b01e9
captain-pool <rickdey1998@gmail.com>
2018-04-01 14:53:49 +0530

    Fornt End Ready

--- fossee_ST_WebDev/ST_WebDev/admin.py
+++ fossee_ST_WebDev/ST_WebDev/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import user,foss,tutorial_detail,payment
 
 # Register your models here.
+admin.site.register(user)
+admin.site.register(foss)
+admin.site.register(tutorial_detail)
+admin.site.register(payment)
--- fossee_ST_WebDev/ST_WebDev/models.py
+++ fossee_ST_WebDev/ST_WebDev/models.py
@@ -4,6 +4,14 @@ class user(models.Model):
     name=models.CharField(max_length=500)
     password=models.CharField(max_length=32) #len(MD5 Hash)=32
     email=models.CharField(max_length=200)
-#class foss(models.Model):
-    #
-
+class foss(models.Model):
+	name=models.CharField(max_length=1000)
+class tutorial_detail(models.Model):
+	foss=models.ForeignKey(foss,on_delete=models.CASCADE)
+	tutorial_name=models.CharField(max_length=1000)
+	expected_submission_date=models.DateField()
+	actual_submission_date=models.DateField()
+	contributor=models.ForeignKey(user,on_delete=models.CASCADE)
+class payment(models.Model):
+	contributor=models.ForeignKey(user,on_delete=models.CASCADE)
+	payment=models.IntegerField()
